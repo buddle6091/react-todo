954584d74d1c91a7f25f0a6b982728bc0f377a22
buddle6091 <buddle6091@naver.com>
2022-10-01 23:56:11 +0900

    tailwindcss postcss daisyui setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
+      <h2 className="text-[30px] underline-offset-2">Hello world!</h2>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -28,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
